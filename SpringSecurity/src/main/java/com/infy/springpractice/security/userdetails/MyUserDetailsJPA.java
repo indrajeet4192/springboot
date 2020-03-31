2c696105159790fa43378dd79850b79d4ab9685d
indrajeet4192 <indrajeet4192@gmail.com>
2020-03-31 17:23:22 +0530

    Adding Preconfiguration for JPA

---
+++ SpringSecurity/src/main/java/com/infy/springpractice/security/userdetails/MyUserDetailsJPA.java
@@ -0,0 +1,55 @@
+package com.infy.springpractice.security.userdetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class MyUserDetailsJPA implements UserDetails {
+	private String username;
+
+	public MyUserDetailsJPA(String username) {
+		this.username = username;
+	}
+
+	public MyUserDetailsJPA() {
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+
+	@Override
+	public String getPassword() {
+		return "jpapass";
+	}
+
+	@Override
+	public String getUsername() {
+		return this.username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
